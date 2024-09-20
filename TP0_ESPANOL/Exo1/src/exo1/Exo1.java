ecf63fd0bcfb720d39930deac9ccb832cf7ca41b
baptiste espanol <baptiste.espanol@epfedu.fr>
2024-09-20 17:47:56 +0200

    création d'un nouveau projet versionné

---
+++ TP0_ESPANOL/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/* Nom: ESPANOL
+Prenom: Baptiste
+Groupe: TDB
+Date: 20/09/2024
+*/
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner (System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+    }
+
+}
