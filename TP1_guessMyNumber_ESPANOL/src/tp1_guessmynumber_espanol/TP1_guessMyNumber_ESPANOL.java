617200e34e815f03417c4beb99d5372a01bc6695
baptiste espanol <baptiste.espanol@epfedu.fr>
2024-09-27 11:15:53 +0200

    création d'un nouveau projet versionné

--- TP1_guessMyNumber_ESPANOL/src/tp1_guessmynumber_espanol/TP1_guessMyNumber_ESPANOL.java
+++ TP1_guessMyNumber_ESPANOL/src/tp1_guessmynumber_espanol/TP1_guessMyNumber_ESPANOL.java
@@ -17,32 +17,129 @@ public class TP1_guessMyNumber_ESPANOL {
   
     public static void main(String[] args) {
         
-        /*génration d'un nombre aléatoire*/
-        Random generateurAleat = new Random();
-        int n = generateurAleat.nextInt(200);
         
-        /*demande d'une valeur à l'utilisateur*/
-        int var=0;
+        
+        
+       /* choix de la difficulté*/
+        int diff=0;
         Scanner sc= new Scanner (System.in);
-        System.out.println("Bonjour, saisissez une valeur:");
-        var=sc.nextInt();
+        System.out.println("Bonjour, veuyez choisir une difficulte \n1) Facile \n2) Normal \n3) Difficile \n4) Cauchemar");
+        diff=sc.nextInt();
         
-        while (var<n || var>n){
-            if (var<n){
-                System.out.println("Trop petit");
-                System.out.println("saisissez une autre valeur:");
-            var=sc.nextInt();
-            }
-            else if (var>n){
-                System.out.println("Trop grand");
-                System.out.println("saisissez une autre valeur:");
-            var=sc.nextInt();
-            }
+        while (diff>4 || diff<=0){
+            System.out.println("erreur de saisie");
+            System.out.println("Veuyez choisir une difficulte \n1) Facile \n2) Normal \n3) Difficile \n4) Cauchemar");
+            diff=sc.nextInt();    
         }
-        if (var==n){
-            System.out.println("C'EST GAGNE");
+        
+        /*difficulté facile*/
+        if (diff==1){
+            Random generateurAleat = new Random();
+            int n = generateurAleat.nextInt(50);
+            System.out.println(n);
+            /*demande d'une valeur à l'utilisateur*/
+            int var=0;
+            System.out.println("Saisissez une valeur entre 0 et 50:");
+            var=sc.nextInt();
+        
+            while (var<n || var>n){
+                if (var<n){
+                    if (n-var>=20){
+                        System.out.println("Vraiment trop petit");
+                        System.out.println("saisissez une autre valeur:");
+                        var=sc.nextInt();
+                    }
+                    if (n-var >10 && n-var<20 ){
+                        System.out.println(" trop petit");
+                        System.out.println("saisissez une autre valeur:");
+                        var=sc.nextInt();
+                    }
+                    if (n-var <=10 ){
+                        System.out.println(" Un peu trop petit");
+                        System.out.println("saisissez une autre valeur:");
+                        var=sc.nextInt();
+                    }
+                }
+                if (var>n){
+                    if (var>n+20){
+                        System.out.println("Vraiment trop grand");
+                        System.out.println("saisissez une autre valeur:");
+                        var=sc.nextInt();
+                    }
+                    if (var >n +10 && var<n+20 ){
+                        System.out.println(" trop grand");
+                        System.out.println("saisissez une autre valeur:");
+                        var=sc.nextInt();
+                    }
+                    if (var <=n+10 ){
+                        System.out.println(" Un peu trop grand");
+                        System.out.println("saisissez une autre valeur:");
+                        var=sc.nextInt();
+                    }
+                }
+                
+            }
+            if (var==n){
+                System.out.println("C'EST GAGNE");
+            }
+        
         }
-
+        if (diff==2){
+            Random generateurAleat = new Random();
+            int n = generateurAleat.nextInt(100);
+            System.out.println(n);
+            /*demande d'une valeur à l'utilisateur*/
+            int var=0;
+            System.out.println("Saisissez une valeur entre 0 et 100:");
+            var=sc.nextInt();
+        
+            while (var<n || var>n){
+                if (var<n){
+                    System.out.println("Trop petit");
+                    System.out.println("Saisissez une valeur entre 0 et 100:");
+                    var=sc.nextInt();
+                }
+                if (var>n){
+                    System.out.println("Trop grand");
+                    System.out.println("Saisissez une valeur entre 0 et 100:");
+                    var=sc.nextInt();
+                }
+                
+            }
+            if (var==n){
+                System.out.println("C'EST GAGNE");
+            }
+        
+        }
+        if (diff==3){
+            Random generateurAleat = new Random();
+            int n = generateurAleat.nextInt(100);
+            System.out.println(n);
+            /*demande d'une valeur à l'utilisateur*/
+            int var=0;
+            System.out.println("vous avez 10 coups \n Saisissez une valeur entre 0 et 200:");
+            var=sc.nextInt();
+        
+            for (int i=0; i<=10;i++){
+                if (var<n){
+                    System.out.println("Trop petit");
+                    System.out.println("Saisissez une valeur entre 0 et 200:");
+                    var=sc.nextInt();
+                }
+                if (var>n){
+                    System.out.println("Trop grand");
+                    System.out.println("Saisissez une valeur entre 0 et 200:");
+                    var=sc.nextInt();
+                }
+                if (i==10){
+                    System.out.println("GAME OVER!");
+                }
+                
+            }
+            if (var==n){
+                System.out.println("C'EST GAGNE");
+            }
+        
+        }
     }
-    
 }
