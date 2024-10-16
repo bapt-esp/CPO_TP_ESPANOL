d279818252d8559722f3807b30e2649b518bbc11
baptiste espanol <baptiste.espanol@epfedu.fr>
2024-10-16 11:06:01 +0200

    création d'un nouveau projet versionné

--- TP2_convertisseurObjet_NOM1_NOM2/src/tp2_convertisseurobjet_ESPANOL/Convertisseur.java
+++ TP2_convertisseurObjet_NOM1_NOM2/src/tp2_convertisseurobjet_ESPANOL/Convertisseur.java
@@ -14,38 +14,50 @@ public class Convertisseur {
     public double CelsiusVersKelvin (double tCelsius) {
         double kelv2=273.15;
         kelv2 += tCelsius;
+        System.out.println(kelv2);
+        nbConversions+=1;
         return kelv2;
     }
     public double KelvinVersCelsius (double tKelvin) {
         double kelv=-273.15;
         kelv += tKelvin;
+        System.out.println(kelv);
+        nbConversions+=1;
         return kelv;
     }
     public double FarenheitVersCelsius (double tFar) {
         double Cels=32;
         Cels= (tFar-Cels)*5/9;
+        System.out.println(Cels);
+        nbConversions+=1;
         return Cels;
     }
     public double CelsiusVersFarenheit (double tCels) {
         double far=32;
         far= (tCels*9/5)+far;
+        System.out.println(far);
+        nbConversions+=1;
         return far;
     }
     public double KelvinVersFarenheit(double tfar) {
         double far=32;
         double kelv=273.15;
         far= (tfar-kelv)*9/5+far;
+        System.out.println(far);
+        nbConversions+=1;
         return far;
     }
     public double FarenheitVersKelvin (double tKelv) {
         double far=32;
         double kelv=273.15;
         far= (tKelv-far)*5/9+kelv;
+        System.out.println(far);
+        nbConversions+=1;
         return far;
     }
     @Override
     public String toString () {
-        return "nb de conversions"+ nbConversions;
+        return "nb de conversions "+ nbConversions;
     }
 
 }
--- TP2_convertisseurObjet_NOM1_NOM2/src/tp2_convertisseurobjet_ESPANOL/TP2_convertisseurObjet_ESPANOL.java
+++ TP2_convertisseurObjet_NOM1_NOM2/src/tp2_convertisseurobjet_ESPANOL/TP2_convertisseurObjet_ESPANOL.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurobjet_ESPANOL;
 
+import java.util.Scanner;
+
 /*Nom: ESPANOL
 Prenom: Baptiste
 date: 15/10/24
@@ -13,7 +15,50 @@ public class TP2_convertisseurObjet_ESPANOL {
 
     
     public static void main(String[] args) {
-        Convertisseur calc1 =new Convertisseur(1);
+        
+        double var=0;
+        int ope=0;
+                
+        Scanner sc= new Scanner (System.in);
+        System.out.println("Bonjour, saisissez une valeur:");
+        var=sc.nextDouble();
+        System.out.println("Saisissez la conversion que vous souhaitez effectuer : \n 1) Celsius => Kelvin \n 2) Kelvin => Celsius \n 3) Farenhiet => Celsius \n 4) Celsius => Farenheit \n 5) Kelvin => Farenheit \n 6) Farenheit => Kelvin" );
+        ope=sc.nextInt();
+        
+        while (ope>6 || ope<=0){
+            System.out.println("erreur de saisie");
+            System.out.println("Saisissez la conversion que vous souhaitez effectuer : \n 1) Celsius => Kelvin \n 2) Kelvin => Celsius \n 3) Farenhiet => Celsius \n 4) Celsius => Farenheit \n 5) Kelvin => Farenheit \n 6) Farenheit => Kelvin");
+            ope=sc.nextInt();    
+        }
+        
+        if(ope==1){
+            Convertisseur calc1 =new Convertisseur();
+            calc1.CelsiusVersKelvin(var);
+        }
+        if(ope==2){
+            Convertisseur calc1 =new Convertisseur();
+            calc1.KelvinVersCelsius(var);
+        }
+        if(ope==3){
+            Convertisseur calc1 =new Convertisseur();
+            calc1.FarenheitVersCelsius(var);
+        }
+        if(ope==4){
+            Convertisseur calc1 =new Convertisseur();
+            calc1.CelsiusVersFarenheit(var);
+        }
+        if(ope==5){
+            Convertisseur calc1 =new Convertisseur();
+            calc1.KelvinVersFarenheit(var);
+        }
+        if(ope==6){
+            Convertisseur calc1= new Convertisseur();
+            calc1.FarenheitVersKelvin(var);
+        }
+        
+        
+        
+        
     }
     
 }
